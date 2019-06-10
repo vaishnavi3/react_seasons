0984537af5eb4a9fb5ab0c1c9323fc8a93060c17
Vaishnavi <vaishnavi.bhaarde@blimp.co.in>
2019-06-10 17:11:16 +0530

    render seasons from userss location

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+class App extends React.Component {
+  // no need to use constructor to def a state
+  // ..after going thrh babel it will automatically
+  // be converted to constructor
+  state = { lat: null, errorMessage: '' };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude}),
+      err => this.setState({ errorMessage: err.message})
+    );
+  }
+
+  componentDidUpdate() {
+    console.log('component rerendered');
+  }
+
+  // helper method
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay  lat={this.state.lat}/>
+    }
+
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return (
+      <div className='border red'>
+        {this.renderContent()}
+      </div>
+    );
+
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
